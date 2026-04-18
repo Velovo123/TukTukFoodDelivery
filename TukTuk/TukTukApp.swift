ec0f4260aa593e7516a7079067bd8f65fbd9eed2
Anatolii <anatolii.semenchuk@schindler.com>
2026-04-18 18:47:51 +0300

    Initial Commit

---
+++ TukTuk/TukTukApp.swift
@@ -0,0 +1,17 @@
+//
+//  TukTukApp.swift
+//  TukTuk
+//
+//  Created by Anatolii Semenchuk on 18.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct TukTukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
